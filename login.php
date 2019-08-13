b987771130a02a3be53ea828bd0d88c7e54d2ed6
fachrz <rfachru4@gmail.com>
2019-08-13 18:59:58 +0700

    add login view with bootstrap

--- login.php
+++ login.php
@@ -4,18 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Paypon-login</title>
 </head>
 <body>
 <?php
 require_once('config/db_config.php');
-?>
-<form action="" method="post">
-    <input type="email" name="email" placeholder="email">
-    <input type="password" name="password" placeholder="password">
-    <input type="submit" name="login" value="Login">
-</form>
-<p>Nggk punya akun? <a href="register.php">Daftar yuk!!</a></p>
+?> 
+<div class="login-container">
+    <i class="fas fa-money-bill-wave"></i>
+    <h1>Paypon</h1>
+    <form action="" method="post" class="login-form border">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" name="login" class="btn btn-primary btn-block">Submit</button>
+    </form>
+    <p>Nggk punya akun? <a href="register.php">Daftar yuk!!</a></p>
+</div>
 
 <?php
     if (isset($_POST['login'])) {
