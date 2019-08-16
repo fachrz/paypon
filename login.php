f99378516655bf26b9ee7e88cca25fbfa3f4b581
fachrz <rfachru4@gmail.com>
2019-08-16 13:18:29 +0700

    Add online lib

--- login.php
+++ login.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/fontawesome/css/all.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <!-- CSS Link -->
     <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://kit.fontawesome.com/eb8b44741d.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    
     <title>Paypon-login</title>
 </head>
 <body>
@@ -25,7 +31,7 @@ require_once('config/db_config.php');
         <label for="exampleInputPassword1">Password</label>
         <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
-    <button type="submit" name="login" class="btn btn-primary btn-block">Submit</button>
+    <button type="submit" name="login" class="btn btn-primary btn-block">Login</button>
     </form>
     <p>Nggk punya akun? <a href="register.php">Daftar yuk!!</a></p>
 </div>
