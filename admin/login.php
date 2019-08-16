344eb9007b5b96515a69b052f572cfb96a3086bf
fachrz <rfachru4@gmail.com>
2019-08-16 13:29:30 +0700

    Fixing UI

--- admin/login.php
+++ admin/login.php
@@ -4,18 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/fontawesome/css/all.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/style.css">
     <title>Paypon Admin</title>
 </head>
 <body>
 <?php
 require_once('../config/db_config.php');
 ?>
-<form action="" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="password">
-    <input type="submit" name="admin-login" value="Login">
-</form>
-<p>Nggk punya akun? <a href="register.php">Daftar yuk!!</a></p>
+<div class="login-container">
+    <i class="fas fa-money-bill-wave"></i>
+    <h1>Paypon Admin</h1>
+    <form action="" method="post" class="login-form border">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Username</label>
+        <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" name="admin-login" class="btn btn-primary btn-block">Submit</button>
+    </form>
+</div>
 
 <?php
     if (isset($_POST['admin-login'])) {
