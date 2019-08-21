91ac4c48097f8721251a9477a15c0b02dab7ae62
fachrz <rfachru4@gmail.com>
2019-08-21 19:59:27 +0700

    add activity process

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/fontawesome/css/all.css">
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/eb8b44741d.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
@@ -43,7 +45,7 @@
             $confirm_status = $stmt->execute($params);
     
             if ($confirm_status) {
-                $q_accountdata = "SELECT email, jumlah_saldo FROM account WHERE email = :email";
+                $q_accountdata = "SELECT email, saldo FROM account WHERE email = :email";
                 $params = array(
                     ":email" => $topup_data['email'],
                 );
@@ -51,8 +53,8 @@
                 $stmt->execute($params);
                 $account_data = $stmt->fetch(PDO::FETCH_ASSOC);
                 if ($account_data) {
-                    $total_saldo = $account_data['jumlah_saldo'] + $topup_data['jumlah_topup'];
-                    $q_saldoupdate = "UPDATE account SET jumlah_saldo = :jumlah_saldo WHERE email = :email";
+                    $total_saldo = $account_data['saldo'] + $topup_data['jumlah_topup'];
+                    $q_saldoupdate = "UPDATE account SET saldo = :jumlah_saldo WHERE email = :email";
                     $params = array(
                         ":jumlah_saldo" => $total_saldo,
                         ":email" => $account_data['email']
@@ -61,6 +63,15 @@
                     $saldoupdate = $stmt->execute($params);
                     if ($saldoupdate) {
                         print('berhasil di konfirmasi');
+                        $query5 = "INSERT INTO tb_aktivitas VALUES ('', :email, :tipe_aktivitas, :saldo_aktivitas, :tgl_aktivitas)";
+                            $params = array(
+                                ":email" => $topup_data['email'],
+                                ":tgl_aktivitas" => date('Y-m-d H:i:s'),
+                                ":saldo_aktivitas" => $topup_data['jumlah_topup'],
+                                ":tipe_aktivitas" => "top-up",
+                            );
+                            $stmt = $dbh->prepare($query5);
+                            $stmt->execute($params);
                     }else {
                         print('gagal di konfirmasi');
                     }
