8ee48d83be649eb84c4a8460de2ad8088d4a2ee3
fachrz <rfachru4@gmail.com>
2019-08-21 20:05:27 +0700

    connect bank, nothing change just litle bit config

---
+++ api/connect_bank.php
@@ -0,0 +1,27 @@
+<?php
+        require_once('../config/db_config.php');
+        session_start();
+        $email = $_SESSION['user']['email'];
+    ?>
+<?php
+
+            
+            $bank = $_POST['kode-bank'];
+            $rekening = $_POST['no-rekening'];
+            
+            $q_bankregister = "INSERT INTO tb_rekening VALUES (:email, :kode_bank, :nomor_rekening)";
+            $params = array(
+                ":email" => $email,
+                ":kode_bank" => $bank,
+                ":nomor_rekening" => $rekening,
+                
+            );
+            $stmt = $dbh->prepare($q_bankregister);
+            $bankregister = $stmt->execute($params);
+            if ($bankregister) {
+                header("Location: bank.php");   
+            }else{
+                print('Bank gagal di hubungkan');
+            }
+        
+?>
