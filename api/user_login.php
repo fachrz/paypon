4793caf1b50294a15963a5772d75bf92ffbfaae5
fachrz <rfachru4@gmail.com>
2019-08-21 19:57:58 +0700

    add user login api

---
+++ api/user_login.php
@@ -0,0 +1,34 @@
+<?php
+        include '../config/db_config.php';
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $query = "SELECT email, password FROM account WHERE email = :email";
+        $stmt = $dbh->prepare($query);
+
+        $params = array(
+            ":email" => $email,
+        );
+
+        $stmt->execute($params);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if ($user) {
+            if ($password == $user['password']) {
+                session_start();
+                $_SESSION["user"] = $user;
+                $status = array(
+                    "status" => "sukses", 
+                );
+                echo json_encode($status);
+            }else {
+                $status = array(
+                    "status" => "gagal", 
+                );
+                echo json_encode($status);
+            }
+        }else{
+
+        }
+?>
