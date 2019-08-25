7487debb56e747e1deec2d089c4e81158f51ce4a
fachrz <rfachru4@gmail.com>
2019-08-25 21:51:06 +0700

    change form style and function

--- register.php
+++ register.php
@@ -4,48 +4,77 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- CSS Link -->
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://kit.fontawesome.com/eb8b44741d.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="assets/script.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet"> 
+    <title>Paypon Register</title>
 </head>
 <body>
-    <form action="" method="post">
-        <input type="email" name="email" placeholder="Masukan Email">
-        <input type="password" name="password" placeholder="Masukan Password">
-        <input type="text" name="nama" placeholder="Masukan Nama">
-        <input type="text" name="no_telp" placeholder="Masukan Telp">
-        <input type="text" name="alamat" placeholder="Masukan Alamat">
-        <input type="submit" name="register" value="Register">
+<div class="login-container">
+    <h1 class="logo">Pay<span>Pon</span></h1>
+    <div class="alert-container">
+
+    </div>
+    <form action="" method="post" class="register-form border">
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Masukan Email" required>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" name="nama" class="form-control" id="nama" placeholder="Masukan Nama" required>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control" id="password" placeholder="Masukan Password" required>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="form-group">
+                <label for="no_telp">No Telepon</label>
+                <input type="text" name="no_telp" class="form-control" id="no_telp" placeholder="Masukan Nomor Telepon" required>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <input type="text" name="alamat" class="form-control" id="alamat" placeholder="Masukan Alamat" required>
+                </div>
+            </div>
+        </div>
+        <input id="register-submit" type="submit" name="register" class="btn btn-danger btn-block" value="Daftar">
     </form>
-
+    <p class="register-section">Sudah punya akun? <a href="login.php">Login Ajahh!!</a></p>
+</div>
     <?php
-    require_once('config/db_config.php');
-
-    if (isset($_POST['register'])) {
-        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-        $name = filter_input(INPUT_POST, 'nama', FILTER_SANITIZE_STRING);
-        $no_telp = filter_input(INPUT_POST, 'no_telp', FILTER_SANITIZE_STRING);
-        $alamat = filter_input(INPUT_POST, 'alamat', FILTER_SANITIZE_STRING);
-
-        $query = "INSERT INTO account (email, password, nama, no_telp, alamat) VALUES (:email, :password, :nama, :no_telp, :alamat)";
-        $stmt = $dbh->prepare($query);
-
-        $params = array(
-            ":email" => $email,
-            ":password" => $password,
-            ":nama" => $name,
-            ":no_telp" => $no_telp,
-            ":alamat" => $alamat,
-        );
-
-        $user = $stmt->execute($params);
-
-        if ($user) {
-            print('register sukses');
-        }else{
-            print('register gagal');
+        if (isset($_POST['register'])) {
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+            $name = filter_input(INPUT_POST, 'nama', FILTER_SANITIZE_STRING);
+            $no_telp = filter_input(INPUT_POST, 'no_telp', FILTER_SANITIZE_STRING);
+            $alamat = filter_input(INPUT_POST, 'alamat', FILTER_SANITIZE_STRING);
+            
+            if ($email == "" || $password == "" || $name == "" || $no_telp == "" || $alamat == "") {
+                
+            }else {?>
+                <script>user_register("<?= $email ?>", "<?= $password ?>", "<?= $name ?>", "<?= $no_telp ?>", "<?= $alamat ?>")</script>
+            <?php }
         }
+        ?>
 
-    }
-    ?>
+  
+    
 </body>
 </html>
